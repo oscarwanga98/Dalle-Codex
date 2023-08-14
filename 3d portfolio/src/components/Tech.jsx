ed442914cb91f3f18ef46bcea1d6dc5b928373da
oscarwanga98 <87162511+oscarwanga98@users.noreply.github.com>
2023-08-14 10:17:55 +0300

    build finished

--- 3d portfolio/src/components/Tech.jsx
+++ 3d portfolio/src/components/Tech.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { BallCanvas } from "./canvas"
+import { SectionWrapper } from "../hoc"
+import { technologies } from "../constants"
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((thechnology)=>(
+        <div className="w-28 h-28" key={thechnology.name}>
+          <BallCanvas icon={thechnology.icon}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech,'')
